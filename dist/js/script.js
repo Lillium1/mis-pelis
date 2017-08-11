2294ff6f3403f3bfa0bad1d038c0e5453c8b6b48
“Anais <ananavelez@gmail.com>
2017-08-11 10:15:56 -0400

    perfil

--- dist/js/script.js
+++ dist/js/script.js
@@ -20254,7 +20254,34 @@ if (jQuery) {
 })(jQuery);
 
 $(document).ready(function() {
-    $('select').material_select();
-    $(".button-collapse").sideNav();
+	$('select').material_select();
+	$(".button-collapse").sideNav();
+
+
+	/* --- LOCAL STORAGE---- */
+	$('#create-boton').click(function(){ 
+
+		var nom= $('#first_name').val();
+		var nomUs= $('#username').val();    
+		var ciud= $('#country').val();
+
+		localStorage.setItem(“Nombre”, nom); 
+		localStorage.setItem(“UserName”, nomUs);
+		localStorage.setItem(“Ciudad”, ciud);
+
+		/*$('#first_name').val()= ""; 
+		$('#username').val()= "";
+		$('#country').val()= "";  }*/
+
+		var nombre = localStorage.getItem(“Nombre”);
+		var nombreUsuario = localStorage.getItem(“UserName”);
+		var ciudad = localStorage.getItem(“Ciudad”);
+
+		$('#usernamelab').append(nombreUsuario);
+		$('#namelab').append(nombre);
+		$('#ciudadlab').append(ciudad);
+	});
+
 });/*plugin side nav*/
+
 
